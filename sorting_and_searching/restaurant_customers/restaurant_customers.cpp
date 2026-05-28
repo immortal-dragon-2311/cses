1c712684f90ece055a9a200b0a5aee7c7222cbbf
immortal.dragon. <tarakpvs1511@gmail.com>
2026-05-28 19:21:01 +0530

    added solution for restaurant customers

---
+++ sorting_and_searching/restaurant_customers/restaurant_customers.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<pair<int, int>> a;
+
+    /*
+    n - number of customers
+    */
+   int n;
+   cin>> n;
+
+   for (int i = 0; i < n; i++) {
+    int arr, dep;
+
+    cin >> arr >> dep;
+    a.push_back({arr, 0});
+    a.push_back({dep, 1});
+   }
+
+   sort(a.begin(), a.end());
+
+   int c=0, mx = 0;
+
+   for(int i = 0; i<a.size(); i++) {
+    auto x = a[i];
+
+    // cout<< x.first << " " << x.second << endl;
+
+    if(x.second == 0) {
+        c++;
+    } else {
+        c--;
+    }
+    
+    // cout << "c is " << c << endl;
+
+    mx = max(c, mx);
+
+   }
+
+   cout<<mx;
+
+
+    return 0;
+}
